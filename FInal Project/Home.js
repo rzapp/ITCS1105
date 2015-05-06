2ea41a630fd56f1e464c3cbcb6cd6eb710ce3424
rzapp <rzapp1@mail.lakelandcc.edu>
2015-05-06 17:50:23 -0400

    Final Project

--- FInal Project/Home.js
+++ FInal Project/Home.js
@@ -4,108 +4,158 @@ Final Project
 home.js
 Raymond Zapp
 LID 00146825
-May 2015
+6 May 2015
 */
-function Create() 
+function buttonReady()
 {
-    return 
-	{
+    $(".login-button, .create-button, .email-input-button, .element-input-button, .element-add-button, .element-update-button").hover(function () {
+        $(this).css("background-color", "#CAFFFF");
+        $(this).css("color", "#6699FF");
+        $(this).css("cursor", "pointer");
+    }, function () {
+        $(this).css("background-color", "rgba(102,153,255,0.75)");
+        $(this).css("color", "#CAFFFF");
+    });
+}
+function buttonBind()
+{
+    $(".create-button").click(function () {
+        $.ajax(Create());
+    });
+    $(".login-button").click(function () {
+        $.ajax(Login());
+    });
+    $(".element-input-button").click(function () {
+        $.ajax(Element($(".account-output").html(), $(".element-name-text").val(), $(".element-value-text").val()));
+    });
+    $(".element-update-button").click(function () {
+        $.ajax(Element($(".account-output").html(), $(".element-name").html(), $(".element-update-text").val()));
+    });
+    $(".email-input-button").click(function () {
+        $.ajax(Element($(".account-output").html(), "emailadd", $(".email-text").val()));
+    });
+}
+function Create() {
+    return {
         url: "/home/CreateAccount",
-        success: function (response) 
-		{ 
-		  CreateResponse(JSON.parse(response)); 
-		},
         data: 
         {
           "Username": $(".new-username-text").val(),
           "Password": $(".new-password-text").val(),
           "EmailAdd": $(".new-email-text").val(),
           "EmailCon": $(".new-email-confirm-text").val()
-        }
+        },
+        success: function (response) { CreateResponse(JSON.parse(response)); }
     }
 }
-function Login() 
-{
-    return 
-	{
+function Login() {
+    return {
         url: "/home/Login",
-        success: function (response) 
-		{ 
-		LoginResponse(JSON.parse(response));
-		}, 
         data:
         {
           "Username": $(".username-text").val(),
           "Password": $(".password-text").val() 
-        }
+        },
+        success: function (response) { LoginResponse(JSON.parse(response)); }
     }
 }
 function Account()
 {
-    return 
-	{
+    return {
         url: "/home/GetAccountInformation",
-        success: function (response)
-		{ 
-		AccountResponse(JSON.parse(response));
-		},
         data:
         {
-          "Username": $(".username-text").val()
-        }
+            "Username": $(".username-text").val() 
+        },
+        success: function (response) { AccountResponse(JSON.parse(response)); }
+    }
+}
+function Element(user, name, value)
+{
+    if (name == "" || value == "")
+    {
+        $(".error-message-element").html("Please complete above entry.").css("color", "red");
+        return;
+    }
+    return {
+        url: "/home/AddOrUpdateElement",
+        data:
+        {
+            "Username": user,
+            "ElementName": name,
+            "ElementValue": value
+        },
+        success: function (response) {
+            if (((JSON.parse(response)).Message) == !"Success")  
+            {
+                alert("error");   // needs error routine
+            }
+            var payloadObject = JSON.parse(JSON.parse(response).Payload);
+            var accountValues = Object.keys(payloadObject.account);
+            $(".element-name-text").val("");
+            $(".element-value-text").val("");
+            $(".error-message-element").html("Thank you.").css("color", "green");
+      }
     }
 }
 function CreateResponse(response) 
 {
     if (response.Message == "Error") 
-	{
+    {
         if (response.Username == "Invalid") 
-		{
-            alert("Username must be at least 6 characters");
+        {
+            $(".error-message-create").html("Your username must contain at least six characters").css("color", "red");
+            $(".new-username-text").focus();
         }
         else if (response.Username == "Exists") 
-		{
-            alert("Username is already taken");
+        {
+            $(".error-message-create").html("Your username is already in use.").css("color", "red");
+            $(".new-username-text").focus();
         }
         else if (response.Password == "Invalid") 
-		{
-            alert("Password must be at least 6 characters");
+        {
+            $(".error-message-create").html("The password must contain at least six characters").css("color", "red");
+            $(".new-password-text").focus();
         }
         else if (response.EmailAdd == "Invalid") 
-		{
-            alert("Email must contain @");
+        {
+            $(".error-message-create").html("A valid email address is required").css("color", "red");
+            $(".new-email-text").focus()
         }
         else if (response.EmailCon == "Invalid") 
-		{
-            alert("Please enter email address");
+        {
+            $(".error-message-create").html("A matching email address is required").css("color", "red");
+            $(".new-email-confirm-text").focus()
         }
         else if (response.EmailCon == "Mismatch") 
-		{
-            alert("Email address entry does not match");
+        {
+            $(".error-message-create").html("The address does not match. Please re-enter email").css("color", "red");
+            $(".new-email-confirm-text").focus();
         }
     }
     else if (response.Message == "Success") 
-	{
-        alert("Good!");
-        $(".account-output").html($(".new-username-text").val());     // just for TESTING
+    {
+        alert(response.Message);
+        $(".username-text").val($(".new-username-text").val());
+        GetAccountInfo();
         slide();
     }
 }
 function LoginResponse(response) 
 {
     if (response.Message == "Error") 
-	{
+    {
         if (response.Username == "Invalid") 
-		{
-            alert($(".username-text").val() + " is NOT existing user account name");
+        {
+            $(".error-message-login").html($(".username-text").val() + " NOT existing user account name").css("color", "red");
         }
         else if (response.Password == "Wrong") 
-		{
-            alert("Password does not match credentials for " + Username);
+        {
+            $(".error-message-login").html($(".password-text").val() + " does not match credentials for " + $(".username-text").val()).css("color", "red");
         }
     }
     else if (response.Message == "Success") 
-	{
+    {
         alert("Welcome back!");
         GetAccountInfo();
         slide();
@@ -115,7 +165,24 @@ function AccountResponse(response)
 {
     var payload = JSON.parse(response.Payload);
     $(".account-output").html(payload.account.username);  
-    $(".email-text").val(payload.account.password);
+    $(".email-text").val(payload.account.emailadd);  
+    var accountValues = Object.keys(payload.account);        
+    //alert("Element Names: " + accountValues);                // available display of all user Element names
+    $(".first").find(".element-name").html(accountValues[2]);
+    $(".first").find(".element-update-text").val(payload.account[accountValues[2]]);
+    if (accountValues.length > 3)
+    {
+        for (element = 3; element < accountValues.length; element++)
+        {
+            var lastElement = $(".account-display").find("div.row").last();
+            var nextElement = lastElement.clone();
+            nextElement.find(".element-name").html(accountValues[element]);
+            nextElement.find(".element-update-text").val(payload.account[accountValues[element]]);
+            lastElement.after(nextElement);
+        }
+    }
+    buttonReady();
+    buttonBind();
 }
 function GetAccountInfo() 
 {
@@ -124,19 +191,10 @@ function GetAccountInfo()
 }
 function slide() 
 {
-    //$(".SLIDE").toggle("slide", {direction: "left"}, 2400);  /* in and out*/
     $(".slide1").toggle("slide", { direction: "right" }, 2400); /* wrapper for login session is slide1*/
-    $(".slide2").toggle("slide", { direction: "left" }, 2400); /* wrapper for account session is slide2*/
+    $(".slide2").toggle("slide", { direction: "left" }, 2400);  /* wrapper for login session is slide2*/    
 }
-$(document).ready(function () 
-{
-    $(".create-button").click(function () 
-    {
-        $.ajax(Create());
-    });
-
-    $(".login-button").click(function () 
-    {
-        $.ajax(Login());
-    });
+$(document).ready(function () {
+    buttonReady();
+    buttonBind();
 });
